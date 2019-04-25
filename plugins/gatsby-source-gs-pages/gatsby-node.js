a86ae94abf8636af415ffbc00afb017e5ebf413b
Jan Maly <honzamaly@gmail.com>
2019-04-25 17:37:49 +0200

    Added GSPage nodes into gatsby

--- plugins/gatsby-source-gs-pages/gatsby-node.js
+++ plugins/gatsby-source-gs-pages/gatsby-node.js
@@ -8,11 +8,31 @@ exports.sourceNodes = (
 
   const { creds, sheetId, workSheetName, maxRows, maxCols} = configOptions
 
+  const processPage = (page, index) => {
+    const nodeId = createNodeId(`gs-page-${index}`)
+    const nodeContent = JSON.stringify(page)
+    const nodeData = Object.assign({}, page, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `GSPage`,
+        content: nodeContent,
+        contentDigest: createContentDigest(page),
+      }  
+    })
+    return nodeData
+  }
+
   return fetchPagesObject({
     creds, 
     sheetId, 
     workSheetName, 
     maxRows, 
     maxCols
-  }).then(data => console.log(data))
+  }).then(data => {
+    data.forEach( (page, index) => {
+      createNode(processPage(page, index))      
+    })    
+  })
 }
