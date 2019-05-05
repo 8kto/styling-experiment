e9ce46251bc2925bbcfb2d67058ffe61aff1df25
Okto <web@axisful.info>
2019-05-05 23:05:48 +0200

    WIP Add TypeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
 module.exports = {
-    plugins     : [
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            },
-        },
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: 'gatsby-source-gs-pages',
-            options: {
-                sheetId      : '11ziMHrafjFaY9ACiacW9H3SlERUTqsKcujvF1oV0npE',
-                workSheetName: 'Pages',
-                maxRows      : 200,
-                maxCols      : 10,
-                templatePath : `${__dirname}/src/templates/gs-page-template.js`,
-                credsPath    : `${__dirname}/client_secret.json`,
-                baseUrl      : `gs-pages`,
-            },
-        },
-        `gatsby-plugin-sass`
-    ],
-    siteMetadata: {
-        title          : `Styling Experiment`,
-        description    : `Blazing fast styling example playground`,
-        navigationItems: [
-            {path: '/', name: 'Home'},
-            {path: '/about/', name: 'About Us'},
-            {path: '/gs-pages/', name: 'GS Pages'},
-        ],
+  plugins     : [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
     },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-gs-pages',
+      options: {
+        sheetId      : '11ziMHrafjFaY9ACiacW9H3SlERUTqsKcujvF1oV0npE',
+        workSheetName: 'Pages',
+        maxRows      : 200,
+        maxCols      : 10,
+        templatePath : `${__dirname}/src/templates/gs-page-template.tsx`,
+        credsPath    : `${__dirname}/client_secret.json`,
+        baseUrl      : `gs-pages`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+  ],
+  siteMetadata: {
+    title          : `Styling Experiment`,
+    description    : `Blazing fast styling example playground`,
+    navigationItems: [
+      {path: '/', name: 'Home'},
+      {path: '/about/', name: 'About Us'},
+      {path: '/gs-pages/', name: 'GS Pages'},
+    ],
+  },
 }
